0cac9465d3e280b8cf58e8fea297487614ad7117
felicetto13 <felice.laterza93@icloud.com>
2022-07-15 02:14:06 +0200

    fixed some error

--- cart.php
+++ cart.php
@@ -1,22 +1,40 @@
 <?php
+require_once __DIR__."/products.php";
 require_once __DIR__."/cardPayment.php";
-require_once __DIR__."/ccustomer.php";
+require_once __DIR__."/customer.php";
 class Cart {
-    private $products=[];
-    private $card = [];
-    
+    public $products=[];
+
     public function addProduct($product){
         $this->products[]=$product;
-
     }
     public function removeProduct(){
 
     }
-    public function getTotal (){
+    public function getProducts(){
+        /* var_dump($this->products); */
+        foreach ($this->products as $product) {
+            return $product;
+        }
+        
+    }
+    public function getTotal ($cardExpiringDate,$sale){
         $currentYear = new DateTime("now");
         $currentYear = $currentYear->format("Y");
+        $totale = 0;
+        $i=0;
+        $products = $this->getProducts();/* 
+        var_dump($products); */
+        if(count($products)>0){
+            for ($i=0; $i < count($products); $i++) { 
+                $price= $products[$i]->getPrice(); 
+            $totale += $price;
+            }
+        }
         if($cardExpiringDate < $currentYear){
-
+            return "Non puo effetturare transazioni";
+        }else{
+            return $totale-($totale*$sale/100);
         }
     }
 }
--- index.php
+++ index.php
@@ -12,17 +12,19 @@ require_once __DIR__."/customer.php";
 $product1 = new Food("croccantini",10,"2022-7-29");
 $product2 = new Food("Cibo in scatola gattini",16,"2021-1-02");
 $card1= new Card(5342556693402197,2029);
-$card2= new Card(5290110902572197,2027);
+$card2= new Card(5290110902572197,2026);
 $kennel1 = new Kennels("Cuccia Grande",280,"120x60x120");
 $kennel2 = new Kennels("Albero gatti",280,"180x30x80");
 $game1 = new Game("osso finto",25,"Giochi per cani");
 $game2 = new Game("Pupazzo",15,"Giochi per gatti");
 $user1= new Customer("Felice", "Laterza","felice.laterza93@icloud.com", true); 
-$user2= new Customer("Merì", "Aversa","meri_aversa@icloud.com", false); 
+$user2= new Customer("Merì", "Aversa","meri_aversa@icloud.com", true); 
 
 $user1->cart->addProduct([$product1,$product2]);
-
-var_dump($card1);
+$user2->cart->addProduct([$product2,$kennel2,$game1]);
+$user1->cart->getTotal($card1->getCardExpiringDate(),$user1->getSale());
+/* $user2->cart->getTotal($card2->getCardExpiringDate(),$user2->getSale()); */
+/* var_dump($card1);
 var_dump($card2);
 var_dump($product1);
 var_dump($product2);
@@ -31,5 +33,19 @@ var_dump($kennel2);
 var_dump($game1);   
 var_dump($game2);
 var_dump($user1);
-var_dump($user2);
+var_dump($user2); */
+
+    /* foreach ($user1->cart->getProducts() as $product) {
+        var_dump($product);
+    }
+ */
+var_dump($user1->cart->getTotal($card1->getCardExpiringDate(),$user1->getSale()));
+
+echo "---------------------------------------------------------------------";
+echo "---------------------------------------------------------------------";
+echo "---------------------------------------------------------------------";
+var_dump($user2->cart->getProducts());
+var_dump($user2->cart->getTotal($card2->getCardExpiringDate(),$user2->getSale()));
+
+
 ?>
